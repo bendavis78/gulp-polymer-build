a7c960a7f3be077e274093cbd20c6be0ed196090
Ben Davis <bendavis78@gmail.com>
2018-02-08 02:44:08 -0600

    Refactored using promises

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Use this gulp plugin to easily extend the `polymer-cli build` command.
 This will still use your build configs from your polymer.json, so setup
 is quick and easy.
 
-The `createBuildStreams` function returns a javascript object for each
-build, with the key being the build name and the value a vinyl stream
-that can be piped using gulp.
+The `createBuildStreams` function returns a promise that resolves to a
+javascript object for each build, with the key being the build name and
+the value a vinyl stream that can be piped using gulp.
 
 You can override the source stream by supplying `source` in the
 `opts` argument. By default, `polymerProject.sources()` is used.
@@ -21,21 +21,36 @@ const del = require('del');
 const path = require('path');
 const gulp = require('gulp');
 const polymerBuild = require('polymer-build');
-const polymerConfigBuild = require('gulp-polymer-build');
+const configBuild = require('gulp-polymer-build');
 const buildDirectory = 'build';
+
+// promise that waits for stream to end
+function waitFor(stream) {
+  return new Promise((resolve, reject) => {
+    stream.on('end', resolve);
+    stream.on('error', reject);
+  });
+}
 
 // get a PolymerProject object based on your polymer.json
 const polymerProject = new polymerBuild.PolymerProject(require('./polymer.json'));
 
 gulp.task('build', () => {
   console.log(`Deleting ${buildDirectory} directory...`);
-  del([buildDirectory]).then(() => {
-    const builds = polymerConfigBuild.createBuildStreams(polymerProject);
+  del.sync([buildDirectory]);
+  return configBuild.createBuildStreams(polymerProject).then(builds => {
+    let promises = [];
 
     for (let name in builds) {
       let dir = path.join(buildDirectory, name);
       builds[name].pipe(gulp.dest(dir));
+      // more post-processing pipes can be added here
+
+      promises.push(waitFor(builds[name]));
     }
+
+    // ensure gulp waits for all streams to end
+    return Promise.all(promises);
   });
 });
 ```
@@ -45,5 +60,5 @@ gulp.task('build', () => {
     const opts = {};
     opts.sources = polymerProject.sources()
         .pipe(myCustomPipe);
-    const builds = polymerConfigBuild.createBuildStreams(polymerProject, opts);
+    const builds = configBuild.createBuildStreams(polymerProject, opts);
 ```
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+const add = require('gulp-add');
 const polymerBuild = require('polymer-build');
 const path = require('path');
 const mergeStream = require('merge-stream');
@@ -17,100 +18,95 @@ const logger = logging.getLogger('polymer-build');
  * @return {Stream} the final build stream
  */
 function createBuildStream(polymerProject, buildOpts, opts) {
-  const buildName = buildOpts.name || 'default';
-  const optimizeOptions = {css: buildOpts.css, js: buildOpts.js, html: buildOpts.html};
-  // If no name is provided, write directly to the build/ directory.
-  // If a build name is provided, write to that subdirectory.
-  opts.buildDirectory = opts.buildDirectory || 'build';
-  const buildDirectory = path.join(opts.buildDirectory, buildName);
-  logger.debug(`"${buildDirectory}": Building with buildOpts:`, buildOpts);
+  return new Promise(resolve => {
+    const buildName = buildOpts.name || 'default';
+    const optimizeOptions = {css: buildOpts.css, js: buildOpts.js, html: buildOpts.html};
+    // If no name is provided, write directly to the build/ directory.
+    // If a build name is provided, write to that subdirectory.
+    opts.buildDirectory = opts.buildDirectory || 'build';
+    const buildDirectory = path.join(opts.buildDirectory, buildName);
+    logger.debug(`"${buildDirectory}": Building with buildOpts:`, buildOpts);
 
-  // Fork the two streams to guarentee we are working with clean copies of each
-  // file and not sharing object references with other builds.
-  const sources = opts.sources || polymerProject.sources();
-  const sourcesStream = polymerBuild.forkStream(sources);
+    // Fork the two streams to guarentee we are working with clean copies of each
+    // file and not sharing object references with other builds.
+    const sources = opts.sources || polymerProject.sources();
+    const sourcesStream = polymerBuild.forkStream(sources);
 
-  const depsStream = polymerBuild.forkStream(polymerProject.dependencies());
-  const htmlSplitter = new polymerBuild.HtmlSplitter();
-  let buildStream = streams.pipeStreams([
-    mergeStream(sourcesStream, depsStream),
-    htmlSplitter.split(),
-    optimizeStreams.getOptimizeStreams(optimizeOptions),
-    htmlSplitter.rejoin()
-  ]);
-  const compiledToES5 = Boolean(optimizeOptions.js && optimizeOptions.js.compile);
-  if (compiledToES5) {
-    buildStream = buildStream.pipe(polymerProject.addBabelHelpersInEntrypoint())
-      .pipe(polymerProject.addCustomElementsEs5Adapter());
-  }
-  const bundled = Boolean(buildOpts.bundle);
-  if (bundled) {
-    const bundlerOptions = {
-      rewriteUrlsInTemplates: true
-    };
-    if (typeof buildOpts.bundle === 'object') {
-      Object.assign(bundlerOptions, buildOpts.bundle);
+    const depsStream = polymerBuild.forkStream(polymerProject.dependencies());
+    const htmlSplitter = new polymerBuild.HtmlSplitter();
+    let buildStream = streams.pipeStreams([
+      mergeStream(sourcesStream, depsStream),
+      htmlSplitter.split(),
+      optimizeStreams.getOptimizeStreams(optimizeOptions),
+      htmlSplitter.rejoin()
+    ]);
+    const compiledToES5 = Boolean(optimizeOptions.js && optimizeOptions.js.compile);
+    if (compiledToES5) {
+      buildStream = buildStream.pipe(polymerProject.addBabelHelpersInEntrypoint())
+        .pipe(polymerProject.addCustomElementsEs5Adapter());
     }
-    buildStream = buildStream.pipe(polymerProject.bundler(bundlerOptions));
-  }
-  if (buildOpts.insertPrefetchLinks) {
-    buildStream = buildStream.pipe(polymerProject.addPrefetchLinks());
-  }
-  buildStream.once('data', () => {
-    logger.info(`(${buildName}) Building...`);
-  });
-  if (buildOpts.basePath) {
-    let basePath = buildOpts.basePath === true ? buildName : buildOpts.basePath;
-    if (!basePath.startsWith('/')) {
-      basePath = '/' + basePath;
-    }
-    if (!basePath.endsWith('/')) {
-      basePath += '/';
-    }
-    buildStream = buildStream.pipe(polymerProject.updateBaseTag(basePath));
-  }
-  if (buildOpts.addPushManifest) {
-    buildStream = buildStream.pipe(polymerProject.addPushManifest());
-  }
-
-  // If a service worker was requested, parse the service worker config file
-  // while the build is in progress. Loading the config file during the build
-  // saves the user ~300ms vs. loading it afterwards.
-  const swPrecacheConfigPath = path.resolve(
-    polymerProject.config.root, buildOpts.swPrecacheConfig || 'sw-precache-config.js');
-
-  let loadSwConfig;
-
-  if (buildOpts.addServiceWorker) {
-    loadSwConfig = loadConfig.loadServiceWorkerConfig(swPrecacheConfigPath);
-  } else {
-    loadSwConfig = Promise.resolve(undefined);
-  }
-
-  loadSwConfig.then(swConfig => {
-    // There is nothing left to do, so wait for the build stream to complete.
-    streams.waitFor(buildStream).then(() => {
-      if (buildOpts.addServiceWorker) {
-        logger.debug(`Generating service worker...`);
-        if (swConfig) {
-          logger.debug(`Service worker config found`, swConfig);
-        } else {
-          logger.debug(`No service worker configuration found at ` +
-              `${swPrecacheConfigPath}, continuing with defaults`);
-        }
-        return polymerBuild.addServiceWorker({
-          buildRoot: buildDirectory,
-          project: polymerProject,
-          swPrecacheConfig: swConfig || undefined,
-          bundled: bundled
-        });
+    const bundled = Boolean(buildOpts.bundle);
+    if (bundled) {
+      const bundlerOptions = {
+        rewriteUrlsInTemplates: true
+      };
+      if (typeof buildOpts.bundle === 'object') {
+        Object.assign(bundlerOptions, buildOpts.bundle);
       }
-    }).then(() => {
-      buildStream.emit('done');
+      buildStream = buildStream.pipe(polymerProject.bundler(bundlerOptions));
+    }
+    if (buildOpts.insertPrefetchLinks) {
+      buildStream = buildStream.pipe(polymerProject.addPrefetchLinks());
+    }
+    buildStream.once('data', () => {
+      logger.info(`(${buildName}) Building...`);
     });
-  });
+    if (buildOpts.basePath) {
+      let basePath = buildOpts.basePath === true ? buildName : buildOpts.basePath;
+      if (!basePath.startsWith('/')) {
+        basePath = '/' + basePath;
+      }
+      if (!basePath.endsWith('/')) {
+        basePath += '/';
+      }
+      buildStream = buildStream.pipe(polymerProject.updateBaseTag(basePath));
+    }
+    if (buildOpts.addPushManifest) {
+      buildStream = buildStream.pipe(polymerProject.addPushManifest());
+    }
 
-  return buildStream;
+    // If a service worker was requested, parse the service worker config file
+    // while the build is in progress. Loading the config file during the build
+    // saves the user ~300ms vs. loading it afterwards.
+    const swPrecacheConfigPath = path.resolve(
+      polymerProject.config.root, buildOpts.swPrecacheConfig || 'sw-precache-config.js');
+
+    if (buildOpts.addServiceWorker) {
+      loadConfig.loadServiceWorkerConfig(swPrecacheConfigPath)
+        .then(swConfig => {
+          logger.debug(`Generating service worker...`);
+          if (swConfig) {
+            logger.debug(`Service worker config found`, swConfig);
+          } else {
+            logger.debug(`No service worker configuration found at ` +
+                `${swPrecacheConfigPath}, continuing with defaults`);
+          }
+
+          return polymerBuild.generateServiceWorker({
+            buildRoot: buildDirectory,
+            project: polymerProject,
+            swPrecacheConfig: swConfig || undefined,
+            bundled: bundled
+          }).then(contents => {
+            contents = contents.toString('utf8');
+            buildStream = buildStream.pipe(add('service-worker.js', contents));
+            resolve(buildStream);
+          });
+        });
+    } else {
+      resolve(buildStream);
+    }
+  });
 }
 
 /**
@@ -121,17 +117,26 @@ function createBuildStream(polymerProject, buildOpts, opts) {
  */
 function createBuildStreams(polymerProject, opts) {
   opts = opts || {};
+  return new Promise(resolve => {
+    // TODO: validate build config
+    const buildStreams = {};
+    const builds = polymerProject.config.builds;
 
-  // TODO: validate build config
-  const buildStreams = {};
-  const builds = polymerProject.config.builds;
+    const promises = [];
+    for (let buildOptions of builds) {
+      let name = buildOptions.name;
+      promises.push(createBuildStream(polymerProject, buildOptions, opts)
+        .then(buildStream => {
+          buildStreams[name] = buildStream;
+        })
+      );
+    }
 
-  for (let buildOptions of builds) {
-    let name = buildOptions.name;
-    buildStreams[name] = createBuildStream(polymerProject, buildOptions, opts);
-  }
-
-  return buildStreams;
+    Promise.all(promises).then(() => {
+      resolve(buildStreams);
+    });
+  });
 }
 
 module.exports.createBuildStreams = createBuildStreams;
+module.exports.waitFor = streams.waitFor;
